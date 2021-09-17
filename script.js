8232086f20840f6dd5ec331530e202f6b3e4b5e9
aurelijap <aurelija@mnt.lt>
2021-09-17 15:22:46 +0300

    Talking and Contact blocks

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-alert("Veikia");
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
+
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 100,
+  freeMode: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
