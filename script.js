6e0b1601440a39fe6a856d49322c35cfd8c97b14
aurelijap <aurelija@mnt.lt>
2021-09-18 16:56:52 +0300

    Forma, tabs and contact blocks

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'horizontal',
-//   loop: true,
+// Formos blokas
+function tikrintiFormosDuomenis() {
+  var firstName = document.getElementById('firstname').value;
+  var lastName = document.getElementById('lastname').value;
+  var phone = document.getElementById('phone').value;
+  if (firstName == '' || lastName == '' || phone == '') {
+    alert('Įveskite visus duomenis');
+    return false;
+  }
+  if (isNaN(phone)) {
+    alert('Blogai įvestas numeris');
+    return false;
+  }
+  alert('Duomenys įvesti');
+  var form = document.getElementById('form');
+  form.reset();
+}
 
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
-// });
-
+// Talking blokas
 var swiper = new Swiper('.swiper', {
   slidesPerView: 3,
   spaceBetween: 100,
@@ -29,3 +26,36 @@ var swiper = new Swiper('.swiper', {
     clickable: true,
   },
 });
+
+// Tabs blokas
+var tab = document.getElementById('pirmastabas');
+tab.style.display = 'block';
+function changeTab(target, name) {
+  var tabs = document.getElementsByClassName('tabs-text');
+  var buttons = document.getElementsByClassName('tab');
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = 'none';
+    buttons[i].classList.remove('active');
+  }
+  var tab = document.getElementById(name);
+  tab.style.display = 'block';
+  target.currentTarget.classList.add('active');
+}
+
+// Contact blokas
+function tikrintiContactFormosDuomenis() {
+  var firstNameContact = document.getElementById('firstnamecontact').value;
+  var lastNameContact = document.getElementById('lastnamecontact').value;
+  var phoneContact = document.getElementById('phonecontact').value;
+  if (firstNameContact == '' || lastNameContact == '' || phoneContact == '') {
+    alert('Įveskite visus duomenis');
+    return false;
+  }
+  if (isNaN(phoneContact)) {
+    alert('Blogai įvestas numeris');
+    return false;
+  }
+  alert('Duomenys įvesti');
+  var contactForm = document.getElementById('contactform');
+  contactForm.reset();
+}
